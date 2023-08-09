9ee2d71eb7e4b103f1c244da0cedfee9af7cbad6
Pedro Rendeiro <pedrorendeirozrk@gmail.com>
2023-08-09 16:48:42 -0300

    Nothing special

--- sketch.R
+++ sketch.R
@@ -1,9 +1,16 @@
+# INSTALL PACKAGES
+# install.packages("DT")
 # install.packages("shiny")
 # install.packages("shinythemes")
+
+# Load shiny (to execute runApp and runExample)
 library(shiny)
+
+# Apps
 runApp("main")
 runApp("test")
 runApp("widgets")
 runApp("censusVis")
 
+# Examples
 runExample("03_reactivity")
--- test/app.R
+++ test/app.R
@@ -1,32 +1,14 @@
-library(shiny)
-
-# Define UI ----
-ui <- fluidPage(
-  titlePanel("censusVis"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Create demographic maps with
-               information from the 2010 US Census."),
-      
-      selectInput("select", "Choose a variable to display", 
-                  choices = list("Percent White" = 1, 
-                                 "Percent Black" = 2,
-                                 "Percent Hispanic" = 3, 
-                                 "Percent Asian" = 4),
-                            selected = 1),
-      
-      sliderInput("slider1", "Range of interest:",
-                  min = 0, max = 100, value = c(0, 100)),
+## Only run this example in interactive R sessions
+if (interactive()) {
+  shinyApp(
+    ui = basicPage(
+      textInput("txt", "Enter the text to display below:"),
+      textOutput("text")
+      # verbatimTextOutput("verb")
     ),
-    mainPanel()
+    server = function(input, output) {
+      output$text <- renderText({ input$txt })
+      # output$verb <- renderText({ input$txt })
+    }
   )
-)
-
-# Define server logic ----
-server <- function(input, output) {
-  
 }
-
-# Run the app ----
-shinyApp(ui = ui, server = server)
